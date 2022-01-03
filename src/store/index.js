83f262eba189eba141e4f7f1f5bc792b59ccd99c
pcy <pcydeveloper@gmail.com>
2022-01-03 10:23:46 +0900

    feat: weather store 연결

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import coordinates from './coordinates';
 import time from './time';
+import weather from './weather';
 
 export default combineReducers({
   coordinates,
   time,
+  weather,
 });
